74ad9121b3a94766cd7af8475a8157ced5613e08
Roland Kullet <renaud.bellec.3@gmail.com>
2018-05-17 16:23:35 +0200

    launches list: image and sucess icon

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -8,6 +8,7 @@ import { MatCardModule } from "@angular/material/card";
 import { MatListModule } from "@angular/material/list";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatIconModule } from "@angular/material/icon";
+import { MatDividerModule } from "@angular/material/divider";
 
 @NgModule({
   imports: [CommonModule],
@@ -20,7 +21,8 @@ import { MatIconModule } from "@angular/material/icon";
     MatCardModule,
     MatListModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatDividerModule
   ],
   declarations: []
 })
